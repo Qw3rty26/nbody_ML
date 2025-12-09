97bbf361632a5c73fa3a229d0d102cf0a65eea1f
Qw3rty26 <asarovalerio26@gmail.com>
2025-12-09 23:01:31 +0100

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -6,11 +6,14 @@
   outputs = { nixpkgs, ... }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
-  in {
-    apps.default = {
+  in
+  {
+    apps.${system}.default = {
       type = "app";
       program = "${pkgs.python3}/bin/python";
       args = [ "${./py/main.py}" ];
+      # optional: make print() unbuffered so output shows immediately
+      env = { PYTHONUNBUFFERED = "1"; };
     };
   };
 }
