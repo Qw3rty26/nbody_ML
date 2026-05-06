40b95bd78428b6da41e0c85a64cbea3f33c109bf
Qw3rty26 <asarovalerio26@gmail.com>
2026-05-06 22:22:20 +0200

    fixed #17 by getting the absolute path and saving in base_path in dispatcher.py

--- py/dispatcher.py
+++ py/dispatcher.py
@@ -1,6 +1,7 @@
 from system import System
 
-system = System() 
+system = System()
+base_path = __file__.rsplit("/", 2)[0] #get the base path of application
 
 def update(data):
     system.update(data.get("timestep", 0))
@@ -23,21 +24,22 @@ def clear():
 
 def html(filename): #returns an html file
     try:
-        with open("../HTML/" + filename, "r", encoding="utf-8") as file:
+        with open(base_path + "/HTML/" + filename, "r", encoding="utf-8") as file:
             return file.read(), "text/html"
     except FileNotFoundError:
-        return {"message": filename + "could not be found."}, "application/json"
+        return {"message": filename + " could not be found."}, "application/json"
 
 def css(filename): #returns a css file
     try:
-        with open("../CSS/" + filename, "r", encoding="utf-8") as file:
+        with open(base_path + "/CSS/" + filename, "r", encoding="utf-8") as file:
             return file.read(), "text/css"
     except FileNotFoundError:
-        return {"message": filename + "could not be found."}, "application/json"
+        return {"message": filename + " could not be found."}, "application/json"
 
 def js(filename): #returns a javascript file
     try:
-        with open("../JS/" + filename, "r", encoding="utf-8") as file:
+        with open(base_path + "/JS/" + filename, "r", encoding="utf-8") as file:
             return file.read(), "application/javascript"
     except FileNotFoundError:
-        return {"message": filename + "could not be found."}, "application/json"
+        return {"message": filename + " could not be found."}, "application/json"
+
