f5aef70e1b231689fb249d21677f3934231e7c40
Qw3rty26 <asarovalerio26@gmail.com>
2026-08-08 15:46:24 +0200

    created galactic_potential.py, added new methods to simulation.py and simulation_runner.py

--- py/simulation.py
+++ py/simulation.py
@@ -5,42 +5,74 @@ import rebound
 class Simulation:
 
     def initialize_simulation(self, dt = 1e-3, integrator = "whfast"):
-       self.simulation.t = 0
-       self.simulation.G = 1.0
-       self.simulation.dt = dt
-       self.simulation.softening = 0
-       self.simulation.integrator = integrator
-       self.time_warp = 10
+        self.simulation.t = 0
+        self.simulation.G = 1.0
+        self.simulation.dt = dt
+        self.simulation.softening = 0
+        self.simulation.integrator = integrator
+        self.time_warp = 10
 
     def __init__(self, dt = 1e-3, integrator = "whfast"):
         self.simulation = rebound.Simulation()
         self.cluster_diagnostics = ClusterDiagnostics(self.simulation)
+        self.galactic_potential = None
         self.initialize_simulation(dt, integrator)
 
     def update(self):
-       for _ in range(self.time_warp):
-          self.simulation.integrate(self.simulation.t + self.simulation.dt)
+        for _ in range(self.time_warp):
+            self.simulation.integrate(self.simulation.t + self.simulation.dt)
+
+    def move_cluster(self, moved_x, moved_y, moved_z):
+        for particle in self.simulation.particles:
+            particle.x += moved_x
+            particle.y += moved_y
+            particle.z += moved_z
+
+    def speed_cluster(self, speed_x, speed_y, speed_z):
+        for particle in self.simulation.particles:
+            particle.vx += speed_x
+            particle.vy += speed_y
+            particle.vz += speed_z
+
+    def add_galaxy_forces_wrapper(self, reb_sim):
+        self.galactic_potential.add_galaxy_forces(
+            self.simulation.particles
+        )
+
+    def add_galactic_potential(self, galactic_potential):
+        self.galactic_potential = galactic_potential
+        self.simulation.additional_forces = self.add_galaxy_forces_wrapper
+        self.move_cluster(
+            4 * self.galactic_potential.get_galaxy_radius(),
+            0,
+            0
+        )
+        self.speed_cluster(
+            0,
+            self.galactic_potential.get_cluster_initial_velocity(10 * self.galactic_potential.get_galaxy_radius()),
+            0
+        )
 
     def get_JSON_snapshot(self):
-       #diagnostics = self.cluster_diagnostics.get_snapshot()
-       snapshot = { # returns a JSON object containing an array of entities' data
-          #**diagnostics,
-          "time": self.simulation.t,
-          "dt": self.simulation.dt,
-          "integrator": str(self.simulation.integrator),
-          "entities": [{
-             "id": i,
-             "xPos": p.x,
-             "yPos": p.y,
-             "zPos": p.z,
-             "xVel": p.vx,
-             "yVel": p.vy,
-             "zVel": p.vz,
-             "mass": p.m
-             #"totalenergy": self.cluster_diagnostics.get_entity_total_energy(p)
-          }for i, p in enumerate(self.simulation.particles)]
-       }
-       return snapshot
+        #diagnostics = self.cluster_diagnostics.get_snapshot()
+        snapshot = { # returns a JSON object containing an array of entities' data
+            #**diagnostics,
+            "time": self.simulation.t,
+            "dt": self.simulation.dt,
+            "integrator": str(self.simulation.integrator),
+            "entities": [{
+                "id": i,
+                "xPos": p.x,
+                "yPos": p.y,
+                "zPos": p.z,
+                "xVel": p.vx,
+                "yVel": p.vy,
+                "zVel": p.vz,
+                "mass": p.m
+                #"totalenergy": self.cluster_diagnostics.get_entity_total_energy(p)
+            }for i, p in enumerate(self.simulation.particles)]
+        }
+        return snapshot
 
     def get_XYZV_snapshot(self):
         snapshot = []
@@ -60,7 +92,7 @@ class Simulation:
 
     def save_to_file(self, file_name):
        if file_name is None:
-          raise ValueError("file name cannot be None")
+           raise ValueError("file name cannot be None")
        self.simulation.save_to_file(file_name)
 
     def clear(self):
@@ -70,27 +102,27 @@ class Simulation:
 
     def add_entity(self, xPos=0, yPos=0, zPos=0, xVel=0, yVel=0, zVel=0, mass=0.1):
         self.simulation.add(
-           m = mass,
-           x = xPos,
-           y = yPos,
-           z = zPos,
-           vx = xVel,
-           vy = yVel,
-           vz = zVel
+            m = mass,
+            x = xPos,
+            y = yPos,
+            z = zPos,
+            vx = xVel,
+            vy = yVel,
+            vz = zVel
         )
 
     def remove_entity(self, entity_id):
        self.simulation.remove(entity_id)
 
     def clean_cluster(self):
-       number_of_escaped_entities = 0
-       escaped_entity_ids = self.cluster_diagnostics.get_escaped_entity_ids()
+        number_of_escaped_entities = 0
+        escaped_entity_ids = self.cluster_diagnostics.get_escaped_entity_ids()
 
-       for entity_id in reversed(escaped_entity_ids):
-          self.simulation.remove(entity_id)
-          number_of_escaped_entities += 1
+        for entity_id in reversed(escaped_entity_ids):
+            self.simulation.remove(entity_id)
+            number_of_escaped_entities += 1
 
-       return number_of_escaped_entities
+        return number_of_escaped_entities
 
 
     def move_to_center_of_mass(self):
--- py/simulation_runner.py
+++ py/simulation_runner.py
@@ -3,7 +3,7 @@ from queue import Empty
 
 from simulation import Simulation
 from plummer import Plummer
-
+from galactic_potential import GalacticPotential
 
 def handle_command(command, state):
    if command == "start":
@@ -19,48 +19,55 @@ def handle_command(command, state):
       ...
 
 def run(command_queue, snapshot_queue):
-   #print(f"child process is running", flush=True)
-   NUMBER_OF_STARS = 512
-   RADIUS = 4
-   STAR_MASS = 1.0 / NUMBER_OF_STARS
+    #print(f"child process is running", flush=True)
+    NUMBER_OF_STARS = 1024
+    RADIUS = 4
+    STAR_MASS = 1.0 / NUMBER_OF_STARS
 
-   plummer = Plummer(RADIUS, NUMBER_OF_STARS)
+    plummer = Plummer(RADIUS, NUMBER_OF_STARS)
 
-   END_TIME = 10 * plummer.plummer_radius ** (3 / 2)
-   SLEEP_TIME = 0.016
+    END_TIME = 10 * plummer.plummer_radius ** (3 / 2)
+    SLEEP_TIME = 0.016
 
-   sim = Simulation()
+    sim = Simulation()
 
-   positions, velocities = plummer.generate_plummer_cluster()
+    positions, velocities = plummer.generate_plummer_cluster()
 
-   for x, v in zip(positions, velocities):
-      sim.add_entity(
-         x[0], x[1], x[2],
-         v[0], v[1], v[2],
-         STAR_MASS
-      )
+    for x, v in zip(positions, velocities):
+        sim.add_entity(
+            x[0], x[1], x[2],
+            v[0], v[1], v[2],
+            STAR_MASS
+        )
 
-   state = {
-      "is_running": True,
-      "is_paused": True
-   }
+    galactic_potential = GalacticPotential(
+        plummer_mass = 70,
+        plummer_radius = 4
+    )
 
-   sim.cluster_diagnostics.set_initial_total_energy()
-   snapshot_queue.put(sim.get_JSON_snapshot())
+    sim.add_galactic_potential(galactic_potential)
 
-   while state["is_running"]:
+    state = {
+        "is_running": True,
+        "is_paused": True
+    }
 
-      try:
-         cmd = command_queue.get_nowait()
-         handle_command(cmd, state)
-      except Empty:
-         pass
+    sim.cluster_diagnostics.set_initial_total_energy()
+    snapshot_queue.put(sim.get_JSON_snapshot())
 
-      if sim.simulation.t > END_TIME:
-         break
+    while state["is_running"]:
 
-      if not state["is_paused"]:
-         sim.update()
-         snapshot_queue.put(sim.get_JSON_snapshot())
+        try:
+            cmd = command_queue.get_nowait()
+            handle_command(cmd, state)
+        except Empty:
+            pass
 
-      #time.sleep(SLEEP_TIME)
+        if sim.simulation.t > END_TIME:
+            break
+
+        if not state["is_paused"]:
+            sim.update()
+            snapshot_queue.put(sim.get_JSON_snapshot())
+
+        #time.sleep(SLEEP_TIME)
